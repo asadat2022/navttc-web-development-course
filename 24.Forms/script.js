6aec0163c3f76f2b2c3d4dc8523e9085b11c8971
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-12 13:23:34 +0500

    Rearrange Files

--- 24.Forms/script.js
+++ 24.Forms/script.js
@@ -1,7 +1,29 @@
-$(document).ready(()=>{
-    $('#reg-form').submit(function (event) {
+jQuery(document).ready(() => {
+    jQuery('#reg-form').submit(function (event) {
         event.preventDefault();
-        let name = $('#name').val();
-        $('#label-name').text(name);
+        let name = jQuery('#name').val();
+        jQuery('#label-name').text(name);
+        jQuery('#name').val('');
+
+        // jQuery('#label-name').text(jQuery('#name').val());
+
+        let subject = jQuery('#subject').val();
+        console.log('subject', subject);
+        jQuery('#label-subject').text(subject);
+
+
+
+        let gender = jQuery('input[name="gender"]:checked').val();
+        console.log(gender);
+
+
+
     });
+    $('#name').change((event) => {
+        jQuery('#label-name').text($(event.target).val());
+    })
+
+    $('#name').change(function () {
+        jQuery('#label-name').text($(this).val());
+    })
 });
