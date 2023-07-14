821400adb239f6f5d63adaac6e69c89cc5c1c077
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-14 12:04:08 +0500

    Working on CRUD

--- 24.Forms/script.js
+++ 24.Forms/script.js
@@ -1,21 +1,29 @@
 $(document).ready(() => {
     $('#reg-form').submit(function (event) {
-        event.preventDefault();
-        $('#label-name').text($('#name').val());
-        $('#label-father').text($('#father-name').val());
-        $('#label-cnic').text($('#cnic').val());
-        let inputDate = $('#date').val()
-        let date = new Date(inputDate);
-        $('#label-dob').text(date.toDateString('en-PK'));
-        $('#label-email').text($('#email').val());
-        $('#label-subject').text($('#subject').val());
-        $('#label-gender').text($('input[name="gender"]:checked').val());
-        $(this).trigger("reset");
+        if (!$(this)[0].checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        } else {
+            $('#label-name').text($('#name').val());
+            $('#label-father').text($('#father-name').val());
+            $('#label-cnic').text($('#cnic').val());
+            let inputDate = $('#date').val()
+            let date = new Date(inputDate);
+            $('#label-dob').text(date.toDateString('en-PK'));
+            $('#label-email').text($('#email').val());
+            $('#label-subject').text($('#subject').val());
+            $('#label-gender').text($('input[name="gender"]:checked').val());
+
+            event.preventDefault();
+        }
+
+        $(this).addClass("was-validated");
+        // $(this).trigger("reset");
     });
 
-    $('#test').click(function() {
-        $(this).attr('disabled',true);
-    })
+    // $('#test').click(function () {
+    //     $(this).attr('disabled', true);
+    // })
 });
 
 
