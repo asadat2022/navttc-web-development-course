608783a258973bcaaf6ce52f6b286fa84c3d0c4f
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-08-01 14:01:55 +0500

    php Basic

---
+++ 29.PHP/index.php
@@ -0,0 +1,62 @@
+
+    <?php
+
+    // echo "<h1>Hello</h1>";
+
+    // // $x; 
+
+
+    // $x = "3";
+
+    // $y = "4";
+    // echo $x . $y;
+
+
+    // $std_name;
+    // $stdName = "Waqar";
+
+    // echo "<br>". $stdName ."<br>";
+
+
+
+    // $x = 3;
+    // $y = 4;
+    // $z = $x + $y;
+    // echo  "Sum: ". $z;
+    // $z = $x - $y;
+    // echo  $z;
+    // $z = $x * $y;
+    // echo  $z;
+    // $z = $x / $y;
+
+    // $x = 3;
+    // $y = "3";
+    // $test = ($x === $y) ? "is Equal" : "Not Equal";
+
+    // echo $test;
+    // if (!$test) {
+    //     echo "is Equal";
+    // }
+
+    // $arr = ['test',3,"true"];
+
+    // $arr[]='New';
+
+    // foreach ($arr as $k => $val) {
+    //     # code...
+    //     echo $k ."=>". $val."<br>";
+
+    // }
+
+    // print_r($arr);
+
+
+    $arr = [
+        "id" => 3,
+        'name' => 'Waqar',
+    ];
+
+    // print_r($arr);
+
+    echo $arr['name'];
+    ?>
