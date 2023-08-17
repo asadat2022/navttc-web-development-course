9bd111237d419d85b5dae8e451cd85edf353580b
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-08-17 11:44:19 +0500

    Javascript

---
+++ 33/05.javaScript/script.js
@@ -0,0 +1,24 @@
+// console.log("External");
+
+
+// let x = 2;
+
+// var y = 2;
+// console.log(y);
+
+// var y = 6;
+// console.log(y);
+
+let x = 10, y = "5", a=3, b="3";
+
+const z = (!(x==y) || a==b)&&true;
+let test = x % a;
+let comp = test == 0;
+
+if (comp) {
+    console.log("Even");
+}else if (!comp) {
+    console.log("ODD");
+}
+// console.log(x%a == 0);
+
